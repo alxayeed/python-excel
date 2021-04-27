232f452dde823c29274de027171dedc0e4fa50ec
alxayeed <alxayeed@gmail.com>
2021-04-27 15:30:08 +0600

    write to excel file using panda

---
+++ py_excel/read_by_panda.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df = pd.DataFrame({'States': ['California', 'Florida', 'Montana', 'Colorodo', 'Washington', 'Virginia'],
+                   'Capitals': ['Sacramento', 'Tallahassee', 'Helena', 'Denver', 'Olympia', 'Richmond'],
+                   'Population': ['508529', '193551', '32315', '619968', '52555', '227032']})
+
+df.to_excel('./data/states.xlsx')
